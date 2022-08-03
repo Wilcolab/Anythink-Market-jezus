735fb758d321c11067cb7a3c4d1fb029864e580a
Tanmesh <tanmesh@devops.com>
2022-08-03 21:08:46 +0530

    updated backend url

--- frontend/src/agent.js
+++ frontend/src/agent.js
@@ -5,7 +5,7 @@ const superagent = superagentPromise(_superagent, global.Promise);
 
 const API_ROOT =
   process.env.NODE_ENV !== "production"
-    ? "http://localhost:3000/api"
+    ? "https://anythink-market-jezus-api.herokuapp.com/api"
     : "https://api.anythink.market/api";
 
 const encode = encodeURIComponent;
