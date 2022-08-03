6ae7a5aa0bc2fda88bfcfbe8fb74fe7a80c9968c
Tanmesh <tanmesh@devops.com>
2022-08-03 17:52:56 +0530

    linted files

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -7,32 +7,29 @@ class Banner extends React.Component {
     hideSearchField: true,
   };
   render() {
-      return (
-        <div className="banner text-white">
-          <div className="container p-4 text-center">
-            <img src={logo} alt="banner" />
-            <div>
-              <span>A place to</span>
-                <span
-                  id="get-part"
-                  onClick={(_) => {
-                    this.setState({ hideSearchField: false });
-                  }}
-                >
-                  {" "}
-                  get{" "}
-                </span>
-                <span
-                  hidden={this.state.hideSearchField}
-                >
-                  <SearchBar />
-                </span>
-              <span> the cool stuff.</span>
-            </div>
+    return (
+      <div className="banner text-white">
+        <div className="container p-4 text-center">
+          <img src={logo} alt="banner" />
+          <div>
+            <span>A place to</span>
+            <span
+              id="get-part"
+              onClick={(_) => {
+                this.setState({ hideSearchField: false });
+              }}
+            >
+              {" "}
+              get{" "}
+            </span>
+            <span hidden={this.state.hideSearchField}>
+              <SearchBar />
+            </span>
+            <span> the cool stuff.</span>
           </div>
         </div>
-      );
-    }
+      </div>
+    );
   }
+}
 export default Banner;
-
