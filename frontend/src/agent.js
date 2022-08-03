6280f071576bbfb0f362e4a67f20d14a17a88381
Tanmesh <tanmesh@devops.com>
2022-08-03 22:48:48 +0530

    backen url

--- frontend/src/agent.js
+++ frontend/src/agent.js
@@ -1,15 +1,12 @@
 import superagentPromise from "superagent-promise";
 import _superagent from "superagent";
 
-process.env.BACKEND_URL = "https://anythink-market-jezus-api.herokuapp.com/api";
-
-const BACKEND_URL = process.env.BACKEND_URL;
 const superagent = superagentPromise(_superagent, global.Promise);
 
 const API_ROOT =
   process.env.NODE_ENV !== "production"
-    ? BACKEND_URL
-    : "https://api.anythink.market/api";
+    ? "http://localhost:3000/api"
+    : "${process.env.BACKEND_URL}";
 
 const encode = encodeURIComponent;
 const responseBody = (res) => res.body;
