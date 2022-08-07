77f46220569ca251384907331b2884503388ec7c
Tanmesh <tanmesh@devops.com>
2022-08-07 23:52:58 +0530

    added placeholder

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -36,7 +36,7 @@ const ItemPreview = (props) => {
     >
       <img
         alt="item"
-        src={item.image}
+        src={item.image || "placeholder.png"}
         className="card-img-top item-img"
         style={{ borderRadius: "20px" }}
       />
