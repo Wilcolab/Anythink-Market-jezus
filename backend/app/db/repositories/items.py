f781f45bd95b2c8a922095f2cee33261ea129447
Tanmesh <tanmesh@devops.com>
2022-08-07 19:29:49 +0530

    added placeholder.png

--- backend/app/db/repositories/items.py
+++ backend/app/db/repositories/items.py
@@ -302,6 +302,10 @@ class ItemsRepository(BaseRepository):  # noqa: WPS214
         if not len(result_rows):
             raise Exception(f'No item with slug {slug}')
         title = result_rows[0]['title']
+        if(len(str(item_row["image"])) == 0):
+            image="placeholder.png"
+        else :
+            image=item_row["image"]
 
         return Item(
             id_=item_row["id"],
@@ -309,7 +313,7 @@ class ItemsRepository(BaseRepository):  # noqa: WPS214
             title=title,
             description=item_row["description"],
             body=item_row["body"],
-            image=item_row["image"],
+            image=image,
             seller=await self._profiles_repo.get_profile_by_username(
                 username=seller_username,
                 requested_user=requested_user,
