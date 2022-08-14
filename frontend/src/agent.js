2834e403d9b4b53b3484c6cbe4381139e31a38fa
Tanmesh <tanmesh59@gmail.com>
2022-08-14 16:22:15 +0530

    reduced page limit

--- frontend/src/agent.js
+++ frontend/src/agent.js
@@ -51,7 +51,7 @@ const Tags = {
 const limit = (count, p) => `limit=${count}&offset=${p ? p * count : 0}`;
 const omitSlug = (item) => Object.assign({}, item, { slug: undefined });
 const Items = {
-  all: (page) => requests.get(`/items?${limit(1000, page)}`),
+  all: (page) => requests.get(`/items?${limit(50, page)}`),
   bySeller: (seller, page) =>
     requests.get(`/items?seller=${encode(seller)}&${limit(500, page)}`),
   byTag: (tag, page) =>
