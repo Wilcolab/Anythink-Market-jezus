70a805dff12f961202afd056d43bae72ecb28132
Tanmesh <tanmesh59@gmail.com>
2022-08-18 19:32:50 +0000

    corrected the typo

--- backend/app/db/repositories/tags.py
+++ backend/app/db/repositories/tags.py
@@ -6,7 +6,7 @@ from app.db.repositories.base import BaseRepository
 
 class TagsRepository(BaseRepository):
     async def get_all_tags(self) -> List[str]:
-        tags_row = await queries.get_all_tag(self.connection)
+        tags_row = await queries.get_all_tags(self.connection)
         return [tag[0] for tag in tags_row]
 
     async def create_tags_that_dont_exist(self, *, tags: Sequence[str]) -> None:
